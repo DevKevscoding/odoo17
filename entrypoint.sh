ee408a573c027005091571d23e55e7bf1e8cf598
DevKevscoding <166475973+DevKevscoding@users.noreply.github.com>
2025-06-17 10:48:06 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "🚀 Start Odoo with database init"
+
+exec odoo \
+  -d odoo_postgres_lgqt \
+  -i base \
+  --db_host=dpg-d18gmcadbo4c739lnukg-a \
+  --db_port=5432 \
+  --db_user=odoo_postgres_lgqt_user \
+  --db_password=hKPpflpNEucat3mylNo2hETerE3mnjEm \
+  --http-port=8069 \
+  --without-demo=all \
+  --log-level=info
