bef249a9a3c27e419b3dff3ceb86954d56dd7bd5
DevKevscoding <166475973+DevKevscoding@users.noreply.github.com>
2025-06-18 09:34:24 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-echo "🚀 Start Odoo with database init"
 
+echo "🚀 Lancement du serveur Odoo avec initialisation de la base de données"
+
+# Attente du démarrage de PostgreSQL (optionnel mais recommandé)
+echo "⏳ Attente de PostgreSQL..."
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
+  sleep 1
+done
+echo "✅ PostgreSQL est prêt"
+
+# Lancement d'Odoo avec les paramètres
 exec odoo \
   -d odoo_postgres_cfe9 \
   -i base \
@@ -8,6 +17,7 @@ exec odoo \
   --db_port=5432 \
   --db_user=odoo_postgres_cfe9_user \
   --db_password=pjrToD9oJOtDDdW9Y6fV8Gh21wFoQX8F \
-  --http-port=8069 \
+  --http-port=8070 \
   --without-demo=all \
-  --log-level=info
+  --log-level=info \
+  --dev=all
