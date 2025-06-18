7f063cbeb1b73182e5f2e2484ee3000d6f341532
DevKevscoding <166475973+DevKevscoding@users.noreply.github.com>
2025-06-18 09:05:36 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ exec odoo \
   -d odoo_postgres_cfe9 \
   -i base \
   --db_host=dpg-d1956mfdiees73ah88r0-a \
-  --db_port=5432 \
+  --db_port=5433 \
   --db_user=odoo_postgres_cfe9_user \
   --db_password=pjrToD9oJOtDDdW9Y6fV8Gh21wFoQX8F \
-  --http-port=8069 \
+  --http-port=8070 \
   --without-demo=all \
   --log-level=info
